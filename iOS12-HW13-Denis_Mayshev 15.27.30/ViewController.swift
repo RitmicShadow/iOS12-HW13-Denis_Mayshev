ce18f4a481af3312b9e53f8d4de0f6615fc4ef3f
DenisMayshev <denmay13@mail.ru>
2024-03-15 15:28:03 +0400

    Initial commit

---
+++ iOS12-HW13-Denis_Mayshev 15.27.30/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS12-HW13-Denis_Mayshev
+//
+//  Created by Ден Майшев on 15.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
